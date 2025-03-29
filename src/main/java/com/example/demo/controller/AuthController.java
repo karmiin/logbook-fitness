0e2ef45be8ab19a5ec21d745a286caaec57dec23
karmin <karmintng@gmail.com>
2025-03-29 22:30:15 +0100

    fix crossorigin3

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://93.148.118.44:80")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
